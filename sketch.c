3c76e75b2ebeced0f20295a4b80b248989630948
Anthony Cantellano <AcAntellAno@users.noreply.github.com>
2018-06-25 20:59:05 -0700

    Update sketch.c
    fixed bugs

--- sketch.c
+++ sketch.c
@@ -1,38 +1,35 @@
-#include<Servo.h>;
+#include<Servo.h>
 
-Servo setX;
-Servo setY;
+Servo serX;
+Servo serY;
+
 String serialData;
 
-void setup(){
-  setX.attach(10);
-  setxY.attach(11);
-  Serial.begin(9600)
-  Serial.setTimeout(10); //default length is 1000ms
+void setup() {
+
+  serX.attach(11);
+  serY.attach(10);
+  Serial.begin(9600);
+  Serial.setTimeout(10);
 }
 
-void loop(){
-
-
-
+void loop() {
+  //lol
 }
 
-void serialEvent(){
-	serialData = Serial.readString();
-
-	setX.write(parseDataX(serialData));
-	setY.write(parseDataX(serialData));
+void serialEvent() {
+  serialData = Serial.readString();
+  serX.write(parseDataX(serialData));
+  serY.write(parseDataY(serialData));
 }
 
 int parseDataX(String data){
-	data.remove(data.indexOf("Y"));
-	data.remove(data.indexOf("X"), 1);
-
-	return data.toInt();
+  data.remove(data.indexOf("Y"));
+  data.remove(data.indexOf("X"), 1);
+  return data.toInt();
 }
 
 int parseDataY(String data){
-	data.remove(0, data.indexOf("Y") + 1);
-
-	return data.toInt();
+  data.remove(0,data.indexOf("Y") + 1);
+  return data.toInt();
 }
